004e9cb206640798d3b2763efe464b26414b5834
Hamster Tian <updateing@users.noreply.github.com>
2016-06-13 20:39:48 +0800

    Make ngrok run in background without UI
    * `-log=stdout` will hide the text-based UI, but `-log=/some/file.log` will not.
    * Seems like a bug, but ngrok 1.x does not accept new commits.

--- bin/start_ngrok.sh
+++ bin/start_ngrok.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 # Why do we have a public IPv4 address but can not be reached outside of campus network?
 BASE_DIR=/jffs
-nohup $BASE_DIR/bin/ngrok -log-level=info -log=/tmp/ngrok.log -config=$BASE_DIR/etc/ngrok.yml start-all &
+nohup $BASE_DIR/bin/ngrok -log-level=info -log=stdout -config=$BASE_DIR/etc/ngrok.yml start-all &
 
